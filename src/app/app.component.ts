534180700e7e795b038245ce76969632409342ca
Rafi <quazirafi1@gmail.com>
2020-10-26 13:04:09 +0600

    angular material UI added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,6 @@
 import { Component } from '@angular/core';
 //import { Console } from 'console';
 
-interface Alert {
-  type: string;
-  message: string;
-}
-
-const ALERTS: Alert[] = [{
-    type: 'success',
-    message: 'This is an success alert',
-  }, {
-    type: 'info',
-    message: 'This is an info alert',
-  }, {
-    type: 'warning',
-    message: 'This is a warning alert',
-  }, {
-    type: 'danger',
-    message: 'This is a danger alert',
-  }, {
-    type: 'primary',
-    message: 'This is a primary alert',
-  }, {
-    type: 'secondary',
-    message: 'This is a secondary alert',
-  }, {
-    type: 'light',
-    message: 'This is a light alert',
-  }, {
-    type: 'dark',
-    message: 'This is a dark alert',
-  }
-];
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -48,18 +16,5 @@ export class AppComponent {
     this.err=!this.err;
   }
 
-  alerts: Alert[];
-
-  constructor() {
-    this.reset();
-  }
-
-  close(alert: Alert) {
-    this.alerts.splice(this.alerts.indexOf(alert), 1);
-  }
-
-  reset() {
-    this.alerts = Array.from(ALERTS);
-  }
 
 }
