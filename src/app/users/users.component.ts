40e47fbf454e305f580868a383ce809a7fdba055
Rafi <quazirafi1@gmail.com>
2020-10-27 09:47:25 +0600

    child to parent component

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-users',
@@ -7,11 +7,16 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  @Input() hero;
+  @Output() parentFunction: EventEmitter<any> =new EventEmitter()
 
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+  sendData(){
+    let data={name:'Rafi', age:'22'}
+    this.parentFunction.emit(data)
   }
 
 }
