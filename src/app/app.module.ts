3d38b2bc3ca80fa3574a194c7be2a25c2d26680a
Rafi <quazirafi1@gmail.com>
2020-10-27 10:50:20 +0600

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,14 +11,16 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatButtonModule } from '@angular/material/button';
-import { UsersComponent } from './users/users.component'
+import { UsersComponent } from './users/users.component';
+import { AdminComponent } from './admin/admin.component'
 @NgModule({
   declarations: [
     AppComponent,
     UserListComponent,
     HeaderComponent,
     FooterComponent,
-    UsersComponent
+    UsersComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
